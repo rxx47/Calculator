e157962e4ecb5ac6392fd0726dbed9f53c1614bd
Rui Xin <rui.xin@duke.edu>
2019-09-09 12:11:07 -0400

    add a while loop

--- calculator.py
+++ calculator.py
@@ -1,7 +1,12 @@
 def interface():
     print("My calculator program")
+    keep_running = True
+    while keep_running
     print("option:")
     print("9 - Quit")
     choice = input("Enter your choice: ")
     if choice == '9':
         return
+        
+if _name_ == "_main_":
+        interface()
